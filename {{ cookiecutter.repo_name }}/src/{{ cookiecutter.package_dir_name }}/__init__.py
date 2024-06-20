518d11214be0cb733fea942c56da0cbbee08adfa
Tieqiong Zhang <tz2600@columbia.edu>
2024-06-20 14:57:33 -0400

    <package>/ __init__.py and version.py

--- {{ cookiecutter.repo_name }}/src/{{ cookiecutter.package_dir_name }}/__init__.py
+++ {{ cookiecutter.repo_name }}/src/{{ cookiecutter.package_dir_name }}/__init__.py
@@ -1,4 +1,24 @@
-from ._version import get_versions
+#!/usr/bin/env python
+##############################################################################
+#
+# (c) {% now 'utc', '%Y' %} The Trustees of Columbia University in the City of New York.
+# All rights reserved.
+#
+# File coded by: Billinge Group members and community contributors.
+#
+# See GitHub contributions for a more detailed list of contributors.
+# https://github.com/{{ cookiecutter.github_org }}/{{ cookiecutter.repo_name }}/graphs/contributors
+#
+# See LICENSE.rst for license information.
+#
+##############################################################################
 
-__version__ = get_versions()["version"]
-del get_versions
+"""{{ cookiecutter.project_short_description }}"""
+
+# package version
+from {{ cookiecutter.project_name }}.version import __version__
+
+# silence the pyflakes syntax checker
+assert __version__ or True
+
+# End of file
