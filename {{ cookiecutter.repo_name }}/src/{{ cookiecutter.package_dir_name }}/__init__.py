701c698dcd7bde016a881c6360d574ecd1201c01
Tieqiong Zhang <tz2600@columbia.edu>
2024-06-20 11:59:45 -0400

    <package>/ __init__.py and version.py

--- {{ cookiecutter.repo_name }}/src/{{ cookiecutter.package_dir_name }}/__init__.py
+++ {{ cookiecutter.repo_name }}/src/{{ cookiecutter.package_dir_name }}/__init__.py
@@ -1,4 +1,23 @@
-from ._version import get_versions
+#!/usr/bin/env python
+##############################################################################
+#
+# (c) 2024 The Trustees of Columbia University in the City of New York.
+# All rights reserved.
+#
+# File coded by: {{ cookiecutter.author_name }}
+#
+# See AUTHORS.rst for a list of people who contributed.
+# See LICENSE.rst for license information.
+#
+##############################################################################
 
-__version__ = get_versions()["version"]
-del get_versions
+"""Smaller shared functions for use by other diffpy packages.
+"""
+
+# package version
+from {{ cookiecutter.project_name }}.version import __version__
+
+# silence the pyflakes syntax checker
+assert __version__ or True
+
+# End of file
