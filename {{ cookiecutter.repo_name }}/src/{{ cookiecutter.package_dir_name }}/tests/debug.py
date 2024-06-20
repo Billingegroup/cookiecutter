cac69e8d71705dcf1fd7358a760990024019e240
Caden Myers <cjm2304@columbia.edu>
2024-06-20 15:51:50 -0400

    debug fix

---
+++ {{ cookiecutter.repo_name }}/src/{{ cookiecutter.package_dir_name }}/tests/debug.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+##############################################################################
+#
+# (c) {% now 'utc', '%Y' %} The Trustees of Columbia University in the City of New York.
+# All rights reserved.
+#
+# File coded by: Billinge Group members and community contributors.
+#
+# See GitHub contributions for a more detailed list of contributors.
+# https://github.com/{{ cookiecutter.full_name }}/{{ cookiecutter.repo_name }}/graphs/contributors
+#
+# See LICENSE.rst for license information.
+#
+##############################################################################
+
+"""
+Convenience module for debugging the unit tests using
+
+python -m {{ cookiecutter.project_name }}.tests.debug
+
+Exceptions raised by failed tests or other errors are not caught.
+"""
+
+
+if __name__ == "__main__":
+    import sys
+
+    from {{ cookiecutter.project_name }}.tests import testsuite
+
+    pattern = sys.argv[1] if len(sys.argv) > 1 else ""
+    suite = testsuite(pattern)
+    suite.debug()
+
+
+# End of file
