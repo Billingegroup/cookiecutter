fa482d4661a6b95217ebcf155793ae5ab10235ab
Sangjoon Bob Lee <bobleesj@gmail.com>
2025-01-03 13:22:41 -0500

    chore: add deps under conda.txt and pip.txt, news

---
+++ news/cli-entry-point.rst
@@ -0,0 +1,23 @@
+**Added:**
+
+* Use `cookie` command to initiate a new project with bg-cookiecutter by configuring an entry point to `run_cookiecutter` function in `cli.py` file.
+
+**Changed:**
+
+* <news item>
+
+**Deprecated:**
+
+* <news item>
+
+**Removed:**
+
+* <news item>
+
+**Fixed:**
+
+* <news item>
+
+**Security:**
+
+* <news item>
--- src/bg_cookiecutter/cli.py
+++ src/bg_cookiecutter/cli.py
@@ -1,10 +1,9 @@
-# cli.py
-
 import subprocess
+
 
 def run_cookiecutter():
     try:
-        # Run the cookiecutter command with your desired template
+        # Run cookiecutter with the Billingegroup template
         subprocess.run(["cookiecutter", "https://github.com/Billingegroup/cookiecutter"], check=True)
     except subprocess.CalledProcessError as e:
         print(f"Failed to run cookiecutter: {e}")
