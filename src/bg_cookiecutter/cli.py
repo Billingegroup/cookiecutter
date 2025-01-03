7911c5f7acf279e7782a113f28a4a19a53fa6d9a
Sangjoon Bob Lee <bobleesj@gmail.com>
2025-01-03 13:26:09 -0500

    chore: improve error msg with more human-readable

--- src/bg_cookiecutter/cli.py
+++ src/bg_cookiecutter/cli.py
@@ -6,4 +6,4 @@ def run_cookiecutter():
         # Run cookiecutter with the Billingegroup template
         subprocess.run(["cookiecutter", "https://github.com/Billingegroup/cookiecutter"], check=True)
     except subprocess.CalledProcessError as e:
-        print(f"Failed to run cookiecutter: {e}")
+        print(f"Failed to run cookiecutter for the following reason: {e}")
